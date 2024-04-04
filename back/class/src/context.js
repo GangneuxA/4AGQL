b8cec7e6c162460e620da41462bd5d442fc8dbea
julienboisgard <julien@MacBook-Pro-de-Leo.local>
2024-04-04 16:12:27 +0200

    add archi

---
+++ back/class/src/context.js
@@ -0,0 +1,42 @@
+const {jwt} = require('jsonwebtoken');
+
+const getUser = async (token) => {
+  try {
+    if (token) {
+      const user = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+      return user;
+    }
+    return null;
+  } catch (error) {
+    return null;
+  }
+};
+
+const context = async ({ req, res }) => {
+  //   console.log(req.body.operationName);
+  if (req.body.operationName === 'IntrospectionQuery') {
+    // console.log('blocking introspection query..');
+    return {};
+  }
+  // allowing the 'CreateUser' and 'Login' queries to pass without giving the token
+  if (
+    req.body.operationName === 'Login'
+  ) {
+    return {};
+  }
+
+  // get the user token from the headers
+  const token = req.headers.authorization || '';
+
+  // try to retrieve a user with the token
+  const user = await getUser(token);
+
+  if (!user) {
+    throwError('User is not Authenticated');
+  }
+
+  // add the user to the context
+  return { user };
+};
+
+module.exports = {context};
