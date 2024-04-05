9c82ff80e5f5a99543ed5d7e86452bbc7ae592aa
GangneuxA <gangneux.alex@gmail.com>
2024-04-05 15:57:08 +0200

    ee

--- front/src/api/client.js
+++ front/src/api/client.js
@@ -1,21 +1,19 @@
-import {  ApolloClient, HttpLink, InMemoryCache,ApolloLink } from "@apollo/client";
+import {  ApolloClient, HttpLink, InMemoryCache, ApolloLink } from "@apollo/client";
 
 
 const user = new HttpLink({
   uri: "http://localhost:4000/",
-  cache: new InMemoryCache()
 })
 const grade = new HttpLink({
-uri: "http://localhost:4002/",
-cache: new InMemoryCache()
+  uri: "http://localhost:4002/",
 })
 
 const classeroom = new HttpLink({
-uri: "http://localhost:4001/",
-cache: new InMemoryCache()
+  uri: "http://localhost:4001/",
 })
 
 export const client = new ApolloClient({
+  cache: new InMemoryCache(),
   link: ApolloLink.split(
     operation => operation.getContext().clientName === 'user',
     user,
